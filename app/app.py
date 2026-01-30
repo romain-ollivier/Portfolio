0308b9a8f14038b26c47fccfa382012fb4d8a49f
Romain OLLIVIER <rollivier@lendys.fr>
2026-01-30 11:57:39 +0100

    Avancé avant test trad

---
+++ app/app.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template
+import os
+
+
+template_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), '..', 'templates')
+app = Flask(__name__, template_folder=template_dir)
+
+
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
